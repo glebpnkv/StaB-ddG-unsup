10919bf6da6a508f76236c400402768af5dbf15d
Gleb Pankov <g@gpnkv.com>
2025-09-07 15:51:56 +0200

    stability_finetune.py - metrics and logging

--- stability_finetune.py
+++ stability_finetune.py
@@ -14,8 +14,8 @@ from tqdm import tqdm
 from stabddg.model import StaBddG
 from stabddg.mpnn_utils import StructureDataset, ProteinMPNN, parse_PDB
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
 
 
 def validation_step(
@@ -23,9 +23,8 @@ def validation_step(
     ddG_data,
     dataset_valid,
     batch_size=20000,
-    name="val",
     device="cuda"
-):
+) -> dict[str, float | None]:
     val_spearman = []
     val_pearson = []
     all_pred = []
@@ -62,10 +61,10 @@ def validation_step(
     pr, _ = pearsonr(np.concatenate(all_pred), np.concatenate(all_labels))
 
     return {
-        f"{name}_spearman": np.mean(val_spearman),
-        f"{name}_pearson": np.mean(val_pearson),
-        f"{name}_all_spearman": sp,
-        f"{name}_all_pearson": pr,
+        f"spearman": float(np.mean(val_spearman)),
+        f"pearson": float(np.mean(val_pearson)),
+        f"all_spearman": float(sp),
+        f"all_pearson": float(pr),
     }
 
 
@@ -81,6 +80,22 @@ def finetune(
 ):
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     ddG_loss_fn = torch.nn.MSELoss()
+
+    # DataFrame with training, validation and test metrics
+    df_metrics = pd.DataFrame()
+
+    # Directory to save model checkpoints
+    if not os.path.exists(args.model_save_dir):
+        logger.info(f"Creating directory {args.model_save_dir}")
+        os.makedirs(args.model_save_dir)
+
+    # Creating a logging file logs.txt
+    log_path = os.path.join(args.model_save_dir, "logs.txt")
+    file_handler = logging.FileHandler(log_path, mode="w")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(file_handler)
+    logger.info(f"Logging to {log_path}")
 
     for epoch in tqdm(range(args.num_epochs), desc="Epoch"):
         model.train()
@@ -131,41 +146,49 @@ def finetune(
             spearmans.append(sp)
 
         train_metrics = {
-            "train_loss": np.mean(train_sum),
-            "train_spearman": np.mean(spearmans),
+            "loss": float(np.mean(train_sum)),
+            "spearman": float(np.mean(spearmans)),
+            "all_spearman": None,
+            "all_pearson": None,
         }
-        logger.info(f'{print_prefix} train metrics:' + f'{train_metrics}')
+        logger.info(f'{print_prefix}: Training metrics: '
+                    f'{ {k: "{0:0.4f}".format(v) for k, v in train_metrics.items() if v is not None} }')
+
+        df_train_metrics = pd.DataFrame(
+            {"epoch": epoch + 1, "split": "train"} | train_metrics,
+            index=[0]
+        )
+        df_metrics = pd.concat([df_metrics, df_train_metrics], ignore_index=True)
 
         model.eval()
         if (epoch + 1) % args.model_save_freq == 0:
-            if not os.path.exists(args.model_save_dir):
-                os.makedirs(args.model_save_dir)
+            logger.info(f"Saving model checkpoint at epoch {epoch + 1}")
             torch.save(
                 model.pmpnn.state_dict(),
                 f"{args.model_save_dir}/{args.run_name}_epoch{epoch}.pt",
             )
-        if epoch % args.val_freq == 0:
+        if (epoch + 1) % args.val_freq == 0:
             with torch.no_grad():
-                valid_metrics = validation_step(
-                    model,
-                    ddG_data,
-                    dataset_valid,
-                    batch_size=10000,
-                    name="valid",
-                    device=device,
+                valid_metrics = validation_step(model, ddG_data, dataset_valid, batch_size=10000, device=device)
+                logger.info(f'{print_prefix}: Valid metrics: '
+                            f'{ {k: "{0:0.4f}".format(v) for k, v in valid_metrics.items() if v is not None} }')
+                df_valid_metrics = pd.DataFrame(
+                    {"epoch": epoch + 1, "split": "valid"} | valid_metrics,
+                    index=[0]
                 )
-                logger.info(f'{print_prefix} valid metrics:' + f'{valid_metrics}')
+                df_metrics = pd.concat([df_metrics, df_valid_metrics], ignore_index=True)
 
-                test_metrics = validation_step(
-                    model,
-                    ddG_data,
-                    dataset_test,
-                    batch_size=10000,
-                    name="test",
-                    device=device,
+                test_metrics = validation_step(model, ddG_data, dataset_test, batch_size=10000, device=device)
+                logger.info(f'{print_prefix}: Test metrics: '
+                            f'{ {k: "{0:0.4f}".format(v) for k, v in test_metrics.items() if v is not None} }')
+                df_test_metrics = pd.DataFrame(
+                    {"epoch": epoch + 1, "split": "test"} | test_metrics,
+                    index=[0]
                 )
+                df_metrics = pd.concat([df_metrics, df_test_metrics], ignore_index=True)
 
-                logger.info(f'{print_prefix} test metrics:' + f'{test_metrics}')
+            # Saving training metrics as a CSV file
+            df_metrics.to_csv(os.path.join(args.model_save_dir, "metrics.csv"), index=False)
 
             if args.wandb:
                 wandb.log(valid_metrics, step=epoch + 1)
@@ -231,7 +254,6 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
 
     # Dataset preprocessing/loading
-
     # Read split files
     with open("data/rocklin/mega_splits.pkl", "rb") as f:
         splits = pickle.load(f)
@@ -314,7 +336,8 @@ if __name__ == "__main__":
     dataset_test = StructureDataset(pdb_dict_test, truncate=None, max_length=3000)
 
     # Load pre-trained ProteinMPNN
-    device = Accelerator().device
+    accelerator = Accelerator()
+    device = accelerator.device
     logger.info(f"Using device: {device}", )
 
     pmpnn = ProteinMPNN(
@@ -341,7 +364,7 @@ if __name__ == "__main__":
         noise_level=args.noise_level,
         device=device,
     )
-
+    model.compile()
     model.to(device)
 
     # Initialize wandb logging
